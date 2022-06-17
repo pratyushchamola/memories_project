1c2b733a84490fc2a362ce30619b69b25ec5b558
pratyushchamola <69751991+pratyushchamola@users.noreply.github.com>
2022-06-17 13:49:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,6 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user',userRoutes);
 
-//const CONNECTION_URL='mongodb+srv://memories-project:project123123@cluster0.yz6br.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
